663835242992dcc7b190d5fa5f29967e4d19d7ee
Lloyd Atkinson <lloydjatkinson@gmail.com>
2019-04-25 21:38:25 +0100

    Initial

---
+++ LinqIntersectWhereContainsBenchmark/ConsoleApp1/Benchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Runner
+{
+    public class Benchmark
+    {
+        private readonly List<int> _first = new List<int>();
+        private readonly List<int> _second = new List<int>();
+
+        public Benchmark()
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                _first.Add(random.Next(0, 1001));
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                _second.Add(random.Next(0, 1001));
+            }
+        }
+
+        [Benchmark]
+        public void Intersect()
+        {
+            _first.Intersect(_second);
+        }
+
+        public void Where()
+        {
+            _first.Where(x => _second.Contains(x));
+        }
+    }
+}
---
+++ LinqIntersectWhereContainsBenchmark/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using Runner;
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
